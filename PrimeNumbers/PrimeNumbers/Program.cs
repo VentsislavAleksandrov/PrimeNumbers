eacad98d6f2d3a4895fbf0c83e424c8ab8ba3bd5
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-05-12 17:01:59 +0300

    Added logic for printing the matrix on the console

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -53,7 +53,24 @@ namespace PrimeNumbers
 					matrix[i, j] = matrix[0, j] * matrix[i, 0];
                 }
             }
-			;
+
+            for (int i = 0; i < mLength; i++)
+            {
+                for (int j = 0; j < mLength; j++)
+                {
+                    if (matrix[i,j] == 0)
+                    {
+                        Console.Write("\t");
+                    }
+                    else
+                    {
+                        Console.Write($"{matrix[i, j]}\t");
+                    }
+					
+                }
+
+				Console.WriteLine();
+            }
 		}
     }
 }
