e0cee31b4e4630ee2479b03c7b0179993cd30d5b
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-05-12 15:14:53 +0300

    Added Logic for getting the necessary prime numbers.

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PrimeNumbers
 {
@@ -6,7 +7,41 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            var n = int.Parse(Console.ReadLine());
+            var p = 0;
+            var k = 0;
+			var counter = 0;
+            var matrix = new int[n, n];
+			var primeNumbersList = new List<int>();
+
+			for (int i = 2; i <= Math.Sqrt(int.MaxValue); i++)
+			{
+				k = 2;
+				p = 1;
+				while (k < i)
+				{
+					if (i % k == 0)
+					{
+						p = 0;
+						break;
+					}
+					k++;
+				}
+				if (p == 1)
+				{
+					primeNumbersList.Add(i);
+					counter++;
+				}
+                if (counter == n)
+                {
+					break;
+                }
+			}
+
+            for (int i = 0; i < primeNumbersList.Count; i++)
+            {
+				Console.WriteLine(primeNumbersList[i]);
+            }
+		}
     }
 }
