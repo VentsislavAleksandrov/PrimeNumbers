c8cf19a483a3a9e01178fa22fd1146634fdace29
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-05-12 17:37:20 +0300

    Implemented Data Processor class

---
+++ PrimeNumbers/PrimeNumbers/DataProccesor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    public class DataProccesor
+    {
+        private int k;
+        private int p;
+		private int counter = 1;
+		private int mLength;
+        private readonly int[,] matrix;
+        private List<int> primeNumbers;
+
+        public DataProccesor(int[,] matrix, List<int> primeNumbers, int mLength)
+        {
+            this.matrix = matrix;
+            this.primeNumbers = primeNumbers;
+			this.mLength = mLength;
+        }
+
+        public void GetPrimeNumbers()
+        {
+			primeNumbers.Add(0);
+			for (int i = 2; i <= Math.Sqrt(int.MaxValue); i++)
+			{
+				k = 2;
+				p = 1;
+				while (k < i)
+				{
+					if (i % k == 0)
+					{
+						p = 0;
+						break;
+					}
+					k++;
+				}
+				if (p == 1)
+				{
+					primeNumbers.Add(i);
+					counter++;
+				}
+				if (counter == mLength)
+				{
+					break;
+				}
+			}
+        }
+
+		public void FillMatrix()
+        {
+			for (int i = 1; i <= mLength - 1; i++)
+			{
+				matrix[0, i] = primeNumbers[i];
+				matrix[i, 0] = primeNumbers[i];
+			}
+
+			for (int i = 1; i < mLength; i++)
+			{
+				for (int j = 1; j < mLength; j++)
+				{
+					matrix[i, j] = matrix[0, j] * matrix[i, 0];
+				}
+			}
+        }
+    }
+}
--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -9,50 +9,12 @@ namespace PrimeNumbers
         {
             var n = int.Parse(Console.ReadLine());
 			var mLength = n + 1;
-            int p;
-            int k;
-			var counter = 1;
             var matrix = new int[mLength, mLength];
 			var primeNumbersList = new List<int>();
-			primeNumbersList.Add(0);
+            var dataProccesor = new DataProccesor(matrix, primeNumbersList, mLength);
 
-			for (int i = 2; i <= Math.Sqrt(int.MaxValue); i++)
-			{
-				k = 2;
-				p = 1;
-				while (k < i)
-				{
-					if (i % k == 0)
-					{
-						p = 0;
-						break;
-					}
-					k++;
-				}
-				if (p == 1)
-				{
-					primeNumbersList.Add(i);
-					counter++;
-				}
-                if (counter == mLength)
-                {
-					break;
-                }
-			}
-
-            for (int i = 1; i <= n; i++)
-            {
-				matrix[0, i] = primeNumbersList[i];
-				matrix[i, 0] = primeNumbersList[i];
-            }
-
-            for (int i = 1; i < mLength; i++)
-            {
-                for (int j = 1; j < mLength; j++)
-                {
-					matrix[i, j] = matrix[0, j] * matrix[i, 0];
-                }
-            }
+            dataProccesor.GetPrimeNumbers();
+            dataProccesor.FillMatrix();
 
             for (int i = 0; i < mLength; i++)
             {
