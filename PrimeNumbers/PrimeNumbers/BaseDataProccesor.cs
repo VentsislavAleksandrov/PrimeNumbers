d9b5b653eaeb5b071a8787dc721642af8bf3f317
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-05-22 19:18:03 +0300

    Added Abstract class BaseDataProccesor

---
+++ PrimeNumbers/PrimeNumbers/BaseDataProccesor.cs
@@ -0,0 +1,79 @@
+﻿using PrimeNumbers.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    public abstract class BaseDataProccesor: IDataProccesor
+    {
+		private int k;
+		private int p;
+		private int counter = 1;
+		private int mLength;
+		private readonly int[,] matrix;
+		private IList<int> primeNumbers;
+
+		public BaseDataProccesor(int[,] matrix, IList<int> primeNumbers, int mLength)
+		{
+			this.matrix = matrix;
+			this.primeNumbers = primeNumbers;
+			this.mLength = mLength;
+		}
+
+		public void GetPrimeNumbers()
+		{
+			primeNumbers.Add(0);
+			GeneratePrimeNumbers(primeNumbers);
+		}
+
+		public void FillMatrix()
+		{
+			MatrixFill();
+		}
+
+		private void GeneratePrimeNumbers(IList<int> primeNumbers)
+		{
+			for (int i = 2; i <= Math.Sqrt(int.MaxValue); i++)
+			{
+				k = 2;
+				p = 1;
+				while (k < i)
+				{
+					if (i % k == 0)
+					{
+						p = 0;
+						break;
+					}
+					k++;
+				}
+				if (p == 1)
+				{
+					primeNumbers.Add(i);
+					counter++;
+				}
+				if (counter == mLength)
+				{
+					break;
+				}
+			}
+		}
+
+		private void MatrixFill()
+		{
+			for (int i = 1; i <= mLength - 1; i++)
+			{
+				matrix[0, i] = primeNumbers[i];
+				matrix[i, 0] = primeNumbers[i];
+			}
+
+			for (int i = 1; i < mLength; i++)
+			{
+				for (int j = 1; j < mLength; j++)
+				{
+					matrix[i, j] = matrix[0, j] * matrix[i, 0];
+				}
+			}
+		}
+	}
+}
+
--- PrimeNumbers/PrimeNumbers/DataProccesor.cs
+++ PrimeNumbers/PrimeNumbers/DataProccesor.cs
@@ -1,78 +1,13 @@
-﻿using PrimeNumbers.Contracts;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace PrimeNumbers
 {
-    public class DataProccesor: IDataProccesor
+    public class DataProccesor: BaseDataProccesor
     {
-        private int k;
-        private int p;
-		private int counter = 1;
-		private int mLength;
-        private readonly int[,] matrix;
-        private IList<int> primeNumbers;
-
         public DataProccesor(int[,] matrix, IList<int> primeNumbers, int mLength)
+            :base(matrix,primeNumbers,mLength)
         {
-            this.matrix = matrix;
-            this.primeNumbers = primeNumbers;
-			this.mLength = mLength;
-        }
 
-        public void GetPrimeNumbers()
-        {
-			primeNumbers.Add(0);
-			GeneratePrimeNumbers(primeNumbers);
-        }
-
-		public void FillMatrix()
-        {
-			MatrixFill();
-        }
-
-		private void GeneratePrimeNumbers(IList<int> primeNumbers)
-        {
-			for (int i = 2; i <= Math.Sqrt(int.MaxValue); i++)
-			{
-				k = 2;
-				p = 1;
-				while (k < i)
-				{
-					if (i % k == 0)
-					{
-						p = 0;
-						break;
-					}
-					k++;
-				}
-				if (p == 1)
-				{
-					primeNumbers.Add(i);
-					counter++;
-				}
-				if (counter == mLength)
-				{
-					break;
-				}
-			}
-		}
-
-		private void MatrixFill()
-        {
-			for (int i = 1; i <= mLength - 1; i++)
-			{
-				matrix[0, i] = primeNumbers[i];
-				matrix[i, 0] = primeNumbers[i];
-			}
-
-			for (int i = 1; i < mLength; i++)
-			{
-				for (int j = 1; j < mLength; j++)
-				{
-					matrix[i, j] = matrix[0, j] * matrix[i, 0];
-				}
-			}
-		}
+        }		
     }
 }
